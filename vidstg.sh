8e287a7d22784156e56b91e62539ce338cccf41b
EvanStefan <steve@actlife.gr>
2025-05-08 13:50:13 +0300

    Improve evaluation

--- vidstg.sh
+++ vidstg.sh
@@ -10,3 +10,12 @@ python3 -m torch.distributed.launch \
     TENSORBOARD_DIR output/vidstg \
     | tee tee_log_train.txt
 
+## EVALUATION
+# python3 -m torch.distributed.launch \
+#     --nproc_per_node=2 \
+#     scripts/test_net.py \
+#     --config-file "experiments/vidstg.yaml" \
+#     INPUT.RESOLUTION 100 \
+#     MODEL.WEIGHT "output/vidstg/model_125000.pth" \
+#     OUTPUT_DIR output/vidstg \
+#     | tee tee_log_eval.txt
