f252d89dee2090fe96fc1d363a029940b06d4ce1
EvanStefan <steve@actlife.gr>
2025-05-30 13:09:46 +0300

    Limit frames per sample only during training

--- datasets/data_utils.py
+++ datasets/data_utils.py
@@ -205,18 +205,19 @@ def make_vidstg_input_clip(cfg, split, video_data):
     else:
         sample_slice = list(range(0, len(video_frames)))
     
-    MAX_FRAMES_PER_SAMPLE = 300
-    d = list(range(len(sample_slice) - MAX_FRAMES_PER_SAMPLE))
-    if len(d) > 0:
-        MAX_ITERS = 1000000
-        for i in range(MAX_ITERS + 1):
-            s = random.choice(d)
-            sample_slice_new = sample_slice[s:s+MAX_FRAMES_PER_SAMPLE]
-            if len(np.where(gt_mask[sample_slice_new])[0]) > 0:
-                break
-            assert i < MAX_ITERS, f"ERROR: No tube found in video sample after {MAX_ITERS} tries."
-        # print(f"{len(sample_slice)} [{min(sample_slice)}, {max(sample_slice)}] >> {len(sample_slice_new)} [{min(sample_slice_new)}, {max(sample_slice_new)}]")
-        sample_slice = sample_slice_new
+    if split == 'train':
+        MAX_FRAMES_PER_SAMPLE = 300
+        d = list(range(len(sample_slice) - MAX_FRAMES_PER_SAMPLE))
+        if len(d) > 0:
+            MAX_ITERS = 1000000
+            for i in range(MAX_ITERS + 1):
+                s = random.choice(d)
+                sample_slice_new = sample_slice[s:s+MAX_FRAMES_PER_SAMPLE]
+                if len(np.where(gt_mask[sample_slice_new])[0]) > 0:
+                    break
+                assert i < MAX_ITERS, f"ERROR: No tube found in video sample after {MAX_ITERS} tries."
+            # print(f"{len(sample_slice)} [{min(sample_slice)}, {max(sample_slice)}] >> {len(sample_slice_new)} [{min(sample_slice_new)}, {max(sample_slice_new)}]")
+            sample_slice = sample_slice_new
             
 
     # Need to Sample the input frames to satisfy
